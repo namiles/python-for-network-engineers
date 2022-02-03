b9ca408ba60366b4fdc9c61a819e29b161db1035
Nick Miles <nam686@yahoo.com>
2022-02-03 15:44:20 -0600

    add flow control examples

---
+++ python_fundamentals/flow_control/loops.py
@@ -0,0 +1,21 @@
+
+# range() function can be used in for loops to iterate a specified amount of times
+for i in range(10,50): 
+    print(i) #starts at 10, and ends at 50-1 (49)
+
+print()
+
+for i in range(10,50,5): 
+    print(i) #starts at 10, and ends at 50-1 (49)
+
+print()
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+print()
+
+string = "This is my string"
+for char in string:
+  print(char)
